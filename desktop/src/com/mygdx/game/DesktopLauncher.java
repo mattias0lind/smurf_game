21bc9b2c4d92501e35aaedb2b58acf3574e210cb
Clapped99 <andres.arriagada@hotmail.com>
2022-09-19 08:24:18 +0200

    Added screen-resolution

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,9 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
+		config.useVsync(true);
+		config.setWindowedMode(1280,720);
+		config.setTitle("Smurf Game");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
