84c11842e00c85ec5a792f8da912bd09d5fce983
Max Magnusson <93663642+BrotherofJohn@users.noreply.github.com>
2022-09-20 10:52:43 +0200

    ändrade till 2d vector

--- core/src/MODEL/character.java
+++ core/src/MODEL/character.java
@@ -1,25 +1,34 @@
 package MODEL;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.Vector2;
+//import com.badlogic.gdx.math.Vector3;
 
 public class character{
 
-    private static final int gravity = -15;
-    private Vector3 position;
-    private Vector3 velocity;
+    private int gravity = -15; //tog bort final ifall man i framtiden vill göra en power-up som förändrar gravitation.
+    private Vector2 position; // kanske inte ens bör vara i karaktär klassen.
+    private Vector2 velocity;
     private Texture characterImage;
 
     public character(int x, int y){
-        position = new Vector3(x, y, 0);
-        velocity = new Vector3(0, 0, 0);
-        characterImage = new Texture("insert smurf photo here!");
+        position = new Vector2(x, y);
+        velocity = new Vector2(0, 0);
+        this.characterImage = characterImage;
     }
-    public void update(float dt){
-        velocity.add(0, gravity, 0);
+    public void update(float dt) {
+        velocity.add(0, gravity);
         velocity.scl(dt);
-        position.add(0, velocity.y, 0);
+        position.add(0, velocity.y);
 
-        velocity.scl(1/dt); //dt är står för deltatime alltså har med tidsuppfattning att göra
+        velocity.scl(1 / dt); //dt är står för deltatime alltså har med tidsuppfattning att göra
     }
+        public Vector2 getPosition(){
+            return position;
+
+    }
+
+        public Texture getCharacterImage(){
+            return characterImage;
+        }
 }
--- core/src/com/mygdx/game/States/playState.java
+++ core/src/com/mygdx/game/States/playState.java
@@ -1,4 +1,26 @@
 package com.mygdx.game.States;
 
-public class playState {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class playState extends abstractState {
+
+    public playState(gameStateManager gsm){
+    super(gsm);
+
+    }
+
+    @Override
+    public void handleInput() {
+        
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch db) {
+
+    }
 }
