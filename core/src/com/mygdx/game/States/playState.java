0c94d0f491a78fce5238f688e9e2d0f2e78bcb76
Clapped99 <andres.arriagada@hotmail.com>
2022-09-26 12:03:22 +0200

    - Character now visible in playstate

--- core/src/com/mygdx/game/States/playState.java
+++ core/src/com/mygdx/game/States/playState.java
@@ -1,42 +1,93 @@
 package com.mygdx.game.States;
 
 
+import CONTROLLER.InputProcessor;
 import MODEL.SmurfCharacter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.World;
 import MODEL.CharacterREAL;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 
 public class playState extends abstractState {
     private CharacterREAL smurf;
     private World world;
+    private Stage stage;
+    private InputProcessor inputProcessor = new InputProcessor();
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
 
     public playState(gameStateManager gsm){
     super(gsm);
-    smurf = new SmurfCharacter(world);
-    //cam.setToOrtho(false, Smurf_Game.gameWidth / 2, Smurf_Game.gameHeigth / 2);
+
+    }
+
+    public class MyActor extends Actor {
+
+        private Sprite SmurfTesting = new Sprite (new Texture("smurf.png"));
+
+        @Override
+        public void setBounds(float x, float y, float width, float height) {
+            super.setBounds(SmurfTesting.getX(), SmurfTesting.getY(), SmurfTesting.getWidth(), SmurfTesting.getHeight());
+        }
+
+        @Override
+        public void setTouchable(Touchable touchable) {
+            super.setTouchable(touchable);
+        }
+
+        @Override
+        public void draw(Batch batch, float alpha){
+            SmurfTesting.draw(batch);
+        }
+
+        @Override
+        public boolean addListener(EventListener listener) {
+            return super.addListener(listener);
+        }
+    }
+
+    private void create(){
+        stage = new Stage();
+        MyActor myActor = new MyActor();
+        stage.addActor(myActor);
+        Gdx.input.setInputProcessor(stage);
+        stage.addActor(myActor);
 
     }
 
     @Override
     public void handleInput() {
-        //if(Gdx.input.isKeyPressed(62))
-        //smurf.jump();
+        Gdx.input.setInputProcessor(inputProcessor);
     }
 
     @Override
     public void update(float dt) {
-        //handleInput();
+        handleInput();
     }
 
     @Override
     public void render(SpriteBatch sb) {
-        //sb.setProjectionMatrix(cam.combined);
+        create();
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
         sb.begin();
         sb.end();
     }
 
     @Override
     public void dispose(){
+        stage.dispose();
+        batch.dispose();
+        texture.dispose();
 
     }
 }
