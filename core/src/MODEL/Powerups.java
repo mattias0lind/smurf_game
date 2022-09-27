90555fcedddc4fae66f18450c9fe88e4fb1d6175
Augus <augustgu@net.chalmers.se>
2022-09-27 09:45:24 +0200

    Fixat till så att man kan klicka vänster i CharacterSelectionState samt lite korrigeringar i powerups klasserna

--- core/src/MODEL/Powerups.java
+++ core/src/MODEL/Powerups.java
@@ -5,12 +5,15 @@ import com.badlogic.gdx.math.Rectangle;
 public abstract class Powerups {
 
     private Rectangle rectangle;
+    private String name;
 
-    public Powerups() {
+    public Powerups(String name) {
         rectangle.set((float) (Math.random()*1280), (float) (Math.random()*720), 16, 16);
+        this.name = name;
     }
 
-    public void CollisionAction(CharacterREAL player){
-        //decide action of collision
-    }
+    public String getName() {return name;}
+
+    public void CollisionAction(CharacterREAL player){}
+    //decide action of collision
 }
--- core/src/com/mygdx/game/States/menuState.java
+++ core/src/com/mygdx/game/States/menuState.java
@@ -25,7 +25,7 @@ public class menuState extends abstractState {
                     && Gdx.input.getX() < (1280 / 2)-(playButton.getWidth() / 2) + playButton.getWidth()
                     && Gdx.input.getY() < 440
                     && Gdx.input.getY() > 330){
-                gsm.set(new playState(gsm));
+                gsm.set(new CharacterSelectionState(gsm));
                 dispose();
             }
         }
