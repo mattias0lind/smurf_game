fdb3ed505bf7f90ad328a0d4bf12081b8f47444c
andresarriagadafuentes <andres.arriagada@hotmail.com>
2022-10-03 09:45:13 +0200

    Removed view element from Smurfcharacter
    -Refactoring

--- core/src/MODEL/SmurfCharacter.java
+++ core/src/MODEL/SmurfCharacter.java
@@ -5,10 +5,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class SmurfCharacter extends Character {
-
     public Sprite sprite;
-    public Texture img = new Texture("smurf.png");
-
     public SmurfCharacter(World world) {
         super("Smurf", world, 100);
     }
