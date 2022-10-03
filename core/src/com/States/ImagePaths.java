dc9939c3525e8ef0f6ca64d6a1b273388f371d9f
IsakBorovac <isak.borovac@gmail.com>
2022-10-03 11:03:10 +0200

    Uppdaterat enum klassen

--- core/src/com/States/ImagePaths.java
+++ core/src/com/States/ImagePaths.java
@@ -1,14 +1,46 @@
 package com.States;
 
 public enum ImagePaths {
-    BACKGROUND("BackgroundMap.png"),
+    //Characters
+    SMURF("smurf.png"),
+    SMURFCARD("smurfCard.png"),
+    SMURFLOOKLEFT("smurf_look_left.png"),
+    BLUESMURFPUNCHING("punching_smurf.png"),
+
+    //PLAY & QUIT
+    PLAYBUTTON("play_text.png"),
+    QUITBUTTON("Quitknapp.png"),
+
+    //ARROWS
+    LEFTARROW("leftArrow.png"),
+    RIGHTARROW("rightArrow.png"),
+
+    //HEARTS
+    REDHEART("redHeart.png"),
     THREEGREYHEARTS("3greyHearts.png"),
+
+
+
     EVILSMURF("evilSmurf.png"),
     EVILSMURFCARD("evilSmurfCard.png"),
-    FRAMEBOARD("frameboard.png");
+    LILLAMY("lillamy.png"),
+    LILLAMYCARD("lillamyCard.png"),
+
+    //MAP
+    BACKGROUND("BackgroundMap.png"),
+
+
+    FRAMEBOARD("frameboard.png"),
+    GAMEOVERSCREEN1("game_over_screen_player_1.png"),
+    GAMEOVERSCREEN2("game_over_screen_player_2.png"),
+    MOONGROUND("groundMoon.png"),
+    HEALTHMETER("healthMeter.png"),
+    HEALTHMETERBACKGROUND("healthmeterbackground.png"),
+    ICON("icon.png");
+
+
 
     public final String label;
-
     private ImagePaths(String label) {
         this.label = label;
     }
