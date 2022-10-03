e805558da5f2cf5a585324dccb083ac46b30163f
Augus <augustgu@net.chalmers.se>
2022-10-03 09:26:32 +0200

    tog bort några onördiga klasser

--- core/src/MODEL/CharacterREAL.java
+++ core/src/MODEL/CharacterREAL.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
+import java.util.Objects;
 
 
 public abstract class CharacterREAL {
@@ -20,9 +21,9 @@ public abstract class CharacterREAL {
 
 
     public CharacterREAL(String nameOfCharacter, World world, float hp) {
-        this.name = nameOfCharacter;
-        this.playerMovement = new Player_Movement(world);
-        this.healthBar = new HealthBar(hp);
+        this.name = Objects.requireNonNull(nameOfCharacter);
+        this.playerMovement = Objects.requireNonNull(new Player_Movement(world));
+        this.healthBar = Objects.requireNonNull(new HealthBar(hp));
     }
 
     public void SetInitialPosition(float x, float y) {
