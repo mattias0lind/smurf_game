6b9193f399d4700c6be899f4badb4cf8753e6cdd
Augus <augustgu@net.chalmers.se>
2022-10-04 12:06:42 +0200

    lagt in några kommentarer

--- core/src/states/PlayState.java
+++ core/src/states/PlayState.java
@@ -29,7 +29,7 @@ public class PlayState extends AbstractState {
     private Texture groundMoon = new Texture(ImagePaths.MOONGROUND.label);
     private Sprite backgroundSprite;
 
-    private int x,i = 3,j = 3;
+    private int x, i = 3,j = 3;
 
     private Character characterOne;
     private Character characterTwo;
