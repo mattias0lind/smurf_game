9a97d3c15318a4ffdf8afc1eb6e4f008e1fa6f94
andresarriagadafuentes <andres.arriagada@hotmail.com>
2022-10-13 19:58:36 +0200

    Added specific charactertests

--- core/src/test/test/java/SpecificGameCharactersTest.java
+++ core/src/test/test/java/SpecificGameCharactersTest.java
@@ -1,19 +1,48 @@
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
+import model.EvilSmurfCharacter;
+import model.LillaMyCharacter;
+import model.SanicCharacter;
 import model.SmurfCharacter;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SpecificGameCharactersTest {
+
     @Test
     public void testSmurfCharacter() {
         Vector2 position = new Vector2();
         World world = new World(position,false);
         SmurfCharacter smurf = new SmurfCharacter(world,position);
-        assertTrue(smurf.charactersCurrentDirection() == false);  // The logical check
+        assertTrue(smurf.getNameOfCharacter() == "Smurf");
     }
+
+    @Test
+    public void testSanicCharacter() {
+        Vector2 position = new Vector2();
+        World world = new World(position,false);
+        SanicCharacter sanic = new SanicCharacter(world,position);
+        assertTrue(sanic.getNameOfCharacter() == "sanic");
+    }
+
+    @Test
+    public void testLillayMyCharacter() {
+        Vector2 position = new Vector2();
+        World world = new World(position,false);
+        LillaMyCharacter lillamy = new LillaMyCharacter(world,position);
+        assertTrue(lillamy.getNameOfCharacter() == "lillamy");
+    }
+
+    @Test
+    public void testEvilSmurfCharacter() {
+        Vector2 position = new Vector2();
+        World world = new World(position,false);
+        EvilSmurfCharacter evilsmurf = new EvilSmurfCharacter(world,position);
+        assertTrue(evilsmurf.getNameOfCharacter() == "evilsmurf");
+    }
+
 
 
 }
--- core/src/view/RoundTimer.java
+++ core/src/view/RoundTimer.java
@@ -1,4 +1,4 @@
-package model;
+package view;
 
 import com.badlogic.gdx.utils.Timer;
 
