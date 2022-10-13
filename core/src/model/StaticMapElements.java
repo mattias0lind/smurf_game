5ed995743e6e2acfeb38473324182367907f7e8b
Augus <augustgu@net.chalmers.se>
2022-10-13 14:10:39 +0200

    juster packeten i StaticMapElements

--- core/src/model/StaticMapElements.java
+++ core/src/model/StaticMapElements.java
@@ -1,7 +1,10 @@
 package model;
 
-
-import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 
 public class StaticMapElements {
